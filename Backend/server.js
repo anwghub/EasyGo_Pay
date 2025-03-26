dcfa2dea022c25c773580f6ca27fb6feb5944b96
Anwesa Ghosh <anwesa2021@gmail.com>
2025-03-27 01:50:15 +0530

    db added

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,8 @@
 import express from "express";
-import mongoose from "mongoose";
+import { connectDB } from "./db/connectDB.js";
 import dotenv from "dotenv";
 dotenv.config();
+
 
 const app = express();
 const PORT = 3000;
@@ -13,5 +14,6 @@ app.get("/",(req,res)=>{
 app.use(express.json());
 
 app.listen(PORT,()=>{ 
+    connectDB();
     console.log(`Server is running on port ${PORT}`);
 })
