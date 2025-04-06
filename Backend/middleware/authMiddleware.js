3cb8143f1bafb404dfbcf8a7e23dbe16bace97cf
Anwesa Ghosh <anwesa2021@gmail.com>
2025-04-07 01:42:57 +0530

    registration changed

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -1,5 +1,8 @@
 import jwt from "jsonwebtoken";
 import User from "../models/User.model.js";
+const cors = require("cors");
+app.use(cors()); 
+
 
 const authMiddleware = async (req, res, next) => {
   try {
