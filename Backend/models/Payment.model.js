155096b038993c87a79f495aa9be68aec919d730
Anwesa Ghosh <anwesa2021@gmail.com>
2025-04-09 11:08:51 +0530

    pcklog

--- Backend/models/Payment.model.js
+++ Backend/models/Payment.model.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const paymentSchema = new mongoose.Schema({
     
-    serderWalletId: { type: mongoose.Schema.Types.ObjectId, ref: "Wallet", required: true },
+    senderWalletId: { type: mongoose.Schema.Types.ObjectId, ref: "Wallet", required: true },
     receiverWalletId: { type: mongoose.Schema.Types.ObjectId, ref: "Wallet", required: true },
     amount: { type: Number, required: true },
     status: { type: String, enum: ["pending", "completed", "failed"], default: "pending" }, 
@@ -11,4 +11,4 @@ const paymentSchema = new mongoose.Schema({
 
 });
 
- module.exports = mongoose.model("Paymemt", paymentSchema);  // Exporting the model to use it in the controller
+export default mongoose.model("Payment", paymentSchema);  // Exporting the model to use it in the controller
